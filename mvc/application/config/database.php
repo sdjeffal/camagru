7781f0e314ebd08fd6ad74784314c1cd5ce81f79
sdjeffal <sdjeffal@student.42.fr>
2016-10-28 19:27:33 +0200

    new start mvc

---
+++ mvc/application/config/database.php
@@ -0,0 +1,10 @@
+<?php
+	$DB_DRIVER = 'mysql';
+	$DB_HOST = '127.0.0.1';
+	$DB_PORT = '3307';
+	$DB_NAME = 'camagru';
+	$DB_DSN = $DB_DRIVER.':host='.$DB_HOST.':'.$DB_PORT.';';
+	$DB_USER = 'sdjeffal';
+	$DB_PASSWORD = 'root';
+	$DB_FILE = 'schemaCamagru.sql';
+ ?>
